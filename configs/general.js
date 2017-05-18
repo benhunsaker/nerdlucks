c6c5050f7705dc8a2866855e920e73996545394f
Ben Hunsaker <benhunsaker@us.ibm.com>
2017-05-18 11:00:47 -0500

    added mongod connection and scaffolding for tournaments api

--- configs/general.js
+++ configs/general.js
@@ -1,8 +1,13 @@
+'use strict';
+
+
 const Path = require('path');
 
 const root = Path.join(__dirname, '..');
 
-module.experts = {
+
+module.exports = {
+    dbUrl: 'mongodb://localhost:27017',
     hostname: process.env.HOSTNAME || 'localhost',
     port: process.env.PORT || 8000,
     paths: {
--- configs/webpack.server-watch.js
+++ configs/webpack.server-watch.js
@@ -4,23 +4,10 @@
 const Webpack = require('webpack');
 
 const Config = require('./webpack.server');
-const GeneralConfig = require('./general');
 
 
 Config.cache = true;
 Config.watch = true;
-
-// Config.entry.unshift(
-//     'webpack/hot/poll?1000'
-// );
-
-Config.output.publicPath = 'http://' + GeneralConfig.hostname + ':' + GeneralConfig.port + '/dist';
-
-// Config.plugins = [
-//     new Webpack.BannerPlugin({ banner: 'require("source-map-support").install();', raw: true, entryOnly: false }),
-//     new Webpack.HotModuleReplacementPlugin(),
-//     new Webpack.NoEmitOnErrorsPlugin()
-// ];
 
 
 module.exports = Config;
